c3bbd5bb6e4e99907077c17fa9bd0e6417c63787
amanshivhare <amanshivhare4510@gmail.com>
2025-09-04 23:59:08 +0530

    chore: Updated import.sql file.

--- event-registration/src/main/resources/import.sql
+++ event-registration/src/main/resources/import.sql
@@ -1,12 +1,14 @@
---INSERT INTO public.users VALUES (true, 1, '$2a$10$0seTNRhaACB8YQeSaLf.B.LdoQtiPEWhTpXqYKdj3RCggOMkmOOgi', 'Alice');
---INSERT INTO public.users VALUES (true, 2, '$2a$10$mkb4liXQkCToxTm5vCbzlOFdqLFva4cv44Jds7x1wNRdTaOPa9cam', 'Bob');
---
---INSERT INTO public.authorities VALUES (1, 1, 'ROLE_USER');
---INSERT INTO public.authorities VALUES (2, 2, 'ROLE_USER');
---
---INSERT INTO public.events VALUES ('2025-09-15', 1, 'Spring Boot basics.', 'Hyderabad', 'Spring Boot Workshop');
---INSERT INTO public.events VALUES ('2025-09-15', 2, 'Kafka basics.', 'Bangalore', 'Kafka Workshop');
---
---INSERT INTO public.registrations VALUES (1, 1, '2025-09-04 21:52:30.911563', 1);
---INSERT INTO public.registrations VALUES (2, 2, '2025-09-04 21:53:43.191434', 2);
---
+INSERT INTO public.users VALUES (true, 1, '$2a$10$sNkKH1w1bs2p5Ed6iG/jc.hGMdhlRE.JgsW8pmPql24AdGTLG4nIG', 'Alice');
+INSERT INTO public.users VALUES (true, 2, '$2a$10$0seTNRhaACB8YQeSaLf.B.LdoQtiPEWhTpXqYKdj3RCggOMkmOOgi', 'Bob');
+INSERT INTO public.users VALUES (true, 3, '$2a$10$mkb4liXQkCToxTm5vCbzlOFdqLFva4cv44Jds7x1wNRdTaOPa9cam', 'Admin');
+
+INSERT INTO public.authorities VALUES (1, 1, 'ROLE_USER');
+INSERT INTO public.authorities VALUES (2, 2, 'ROLE_USER');
+INSERT INTO public.authorities VALUES (3, 3, 'ROLE_ADMIN');
+
+INSERT INTO public.events VALUES ('2025-09-15', 1, 'Spring Boot basics.', 'Hyderabad', 'Spring Boot Workshop');
+INSERT INTO public.events VALUES ('2025-09-15', 2, 'Kafka basics.', 'Bangalore', 'Kafka Workshop');
+
+INSERT INTO public.registrations VALUES (1, 1, '2025-09-04 21:52:30.911563', 1);
+INSERT INTO public.registrations VALUES (2, 2, '2025-09-04 21:53:43.191434', 2);
+
